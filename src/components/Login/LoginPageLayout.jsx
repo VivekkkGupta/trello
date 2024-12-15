d1a46f7dfe4136af1699dbc6b1b65912fa342307
Vivek <gupta1035@gmail.com>
2024-12-15 23:59:51 +0530

    removed google login

--- src/components/Login/LoginPageLayout.jsx
+++ src/components/Login/LoginPageLayout.jsx
@@ -29,9 +29,9 @@ function LoginPageLayout() {
             </h1>
             <div className="w-full flex-1 mt-8">
               <div className="flex flex-col items-center">
-                <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                {/* <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
                   <div className="bg-white p-2 rounded-full">
-                    {/* Google Logo */}
+                    Google Logo
                     <svg className="w-4" viewBox="0 0 533.5 544.3">
                       <path
                         d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
@@ -52,7 +52,7 @@ function LoginPageLayout() {
                     </svg>
                   </div>
                   <span className="ml-4">{signInOrSignUpText} with Google</span>
-                </button>
+                </button> */}
 
                 {/* <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
                   <div className="bg-white p-1 rounded-full">
@@ -67,11 +67,11 @@ function LoginPageLayout() {
                 </button> */}
               </div>
 
-              <div className="my-12 border-b text-center">
+              {/* <div className="my-12 border-b text-center">
                 <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
                   Or {signInOrSignUpText.toLowerCase()} with e-mail
                 </div>
-              </div>
+              </div> */}
 
               <div className="mx-auto max-w-xs">
                 {
